3d9b122f079288a99e83633e75105006ab6200b5
Luciano Quercia <luciano.quercia@gmail.com>
2014-06-30 12:51:21 +0200

    mvn funge

--- src/main/java/it/uniba/di/ia/ius/gui/MainWindow.java
+++ src/main/java/it/uniba/di/ia/ius/gui/MainWindow.java
@@ -14,7 +14,6 @@ public class MainWindow {
     private JTextPane textPane;
     private JList jlist;
     private JButton extractButton;
-    private JPanel contentPane;
     private JCheckBox personeCheckBox;
     private JCheckBox indirizziEMailCheckBox;
     private JCheckBox comuniCheckBox;
@@ -23,12 +22,14 @@ public class MainWindow {
     private JCheckBox codiciFiscaliCheckBox;
     private JCheckBox numeriDiTelefonoCheckBox;
     private JButton resetButton;
+    private JPanel contentPane;
     DefaultListModel defaultListModel;
 
     private class MyListCellRenderer extends JLabel implements ListCellRenderer {
         public MyListCellRenderer() {
             setOpaque(true);
         }
+
         public Component getListCellRendererComponent(JList paramlist, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             setText(value.toString());
             if (value.toString().contains("persona")) {
@@ -65,6 +66,7 @@ public class MainWindow {
 
     public MainWindow() {
         frame = new JFrame("Tagger ius");
+        assert contentPane != null;
         frame.setContentPane(contentPane);
 //        this.createMenu();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -112,8 +114,7 @@ public class MainWindow {
                 String message = "Reset all?";
                 String title = "Reset";
                 int reply = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
-                if (reply == JOptionPane.YES_OPTION)
-                {
+                if (reply == JOptionPane.YES_OPTION) {
                     defaultListModel.clear();
                     textPane.setText("");
                 }
@@ -128,31 +129,31 @@ public class MainWindow {
                 defaultListModel.clear();
                 prolog.consult(new Atom("prolog/main.pl"));
                 prolog.retractAll("domanda", 1);
-                Term toAssert = new Compound("domanda", new Term[]{ Util.textToTerm("\"" + textPane.getText() + "\"")} );
-                prolog.asserta( toAssert );
+                Term toAssert = new Compound("domanda", new Term[]{Util.textToTerm("\"" + textPane.getText() + "\"")});
+                prolog.asserta(toAssert);
                 java.util.Hashtable<String, Term>[] hashtables = prolog.allSolutions(new Compound("nextTag", new Term[]{new Variable("Tag")}));
                 for (int i = 0; i < hashtables.length; i++) {
                     Term t = hashtables[i].get("Tag");
-                    if(indirizziEMailCheckBox.isSelected() && t.toString().contains("mail"))
+                    if (indirizziEMailCheckBox.isSelected() && t.toString().contains("mail"))
 
                         defaultListModel.addElement(t);
 
-                    if(personeCheckBox.isSelected() && t.toString().contains("persona"))
+                    if (personeCheckBox.isSelected() && t.toString().contains("persona"))
                         defaultListModel.addElement(t);
 
-                    if(numeriDiTelefonoCheckBox.isSelected() && t.toString().contains("tel"))
+                    if (numeriDiTelefonoCheckBox.isSelected() && t.toString().contains("tel"))
                         defaultListModel.addElement(t);
 
-                    if(comuniCheckBox.isSelected() && t.toString().contains("comune"))
+                    if (comuniCheckBox.isSelected() && t.toString().contains("comune"))
                         defaultListModel.addElement(t);
 
-                    if(valutaCheckBox.isSelected() && t.toString().contains("richiesta"))
+                    if (valutaCheckBox.isSelected() && t.toString().contains("richiesta"))
                         defaultListModel.addElement(t);
 
-                    if(dateCheckBox.isSelected() && t.toString().contains("date"))
+                    if (dateCheckBox.isSelected() && t.toString().contains("date"))
                         defaultListModel.addElement(t);
 
-                    if(codiciFiscaliCheckBox.isSelected() && t.toString().contains("cf"))
+                    if (codiciFiscaliCheckBox.isSelected() && t.toString().contains("cf"))
                         defaultListModel.addElement(t);
                 }
                 JOptionPane.showMessageDialog(null, "Tagger finished");
