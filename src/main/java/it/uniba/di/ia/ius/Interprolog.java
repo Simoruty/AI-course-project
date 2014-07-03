830ec0924df335b615e7f8ded08995119e8b4f84
Luciano Quercia <luciano.quercia@gmail.com>
2014-07-03 11:07:21 +0200

    make config

--- src/main/java/it/uniba/di/ia/ius/Interprolog.java
+++ src/main/java/it/uniba/di/ia/ius/Interprolog.java
@@ -1,10 +1,10 @@
 package it.uniba.di.ia.ius;
 
+import com.declarativa.interprolog.PrologEngine;
+import com.declarativa.interprolog.YAPSubprocessEngine;
+
 import java.io.File;
 import java.util.List;
-import com.declarativa.interprolog.PrologEngine;
-import com.declarativa.interprolog.TermModel;
-import com.declarativa.interprolog.YAPSubprocessEngine;
 
 /**
  * Created by simo
@@ -12,46 +12,46 @@ import com.declarativa.interprolog.YAPSubprocessEngine;
 
 public class Interprolog {
 
-        private static PrologEngine engine;
+    private static PrologEngine engine;
 
-        public Interprolog(String coreConsultPath, String prolog_path) {
+    public Interprolog(String coreConsultPath, String prolog_path) {
 
-            engine = new YAPSubprocessEngine(prolog_path, true);
-            engine.consultAbsolute(new File(coreConsultPath));
-            System.out.println(engine.getPrologVersion());
+        engine = new YAPSubprocessEngine(prolog_path, false);
+        engine.consultAbsolute(new File(coreConsultPath));
+        System.out.println(engine.getPrologVersion());
+    }
+
+    public void close() {
+        engine.shutdown();
+    }
+
+    public void addFiles(List<String> sourceFiles) {
+        // loading files
+        String load = "load_all(['";
+        for (String file : sourceFiles) {
+            load += (file + "','");
         }
 
-        public void close(){
-            engine.shutdown();
-        }
+        load = load.substring(0, load.length() - 2);
+        load += "])";
 
-        public void addFiles(List<String> sourceFiles) {
-            // loading files
-            String load = "load_all(['";
-            for (String file : sourceFiles) {
-                load += (file + "','");
-            }
+        engine.deterministicGoal(load);
+    }
 
-            load = load.substring(0, load.length() - 2);
-            load += "])";
+    public boolean sendCommand(String command) {
+        return engine.deterministicGoal(command);
+    }
 
-            engine.deterministicGoal(load);
-        }
-
-        public boolean sendCommand(String command) {
-            return engine.deterministicGoal(command);
-        }
-
-        public String sendCommand(String command, String var) {
-            String query = command + "(" + var + ") , term_to_atom("+var+",Result)";
-            System.out.println(query);
-            Object[] result = engine.deterministicGoal(query, "[string(Result)]");
-            return result[0].toString();
+    public String sendCommand(String command, String var) {
+        String query = command + "(" + var + ") , term_to_atom(" + var + ",Result)";
+        System.out.println(query);
+        Object[] result = engine.deterministicGoal(query, "[string(Result)]");
+        return result[0].toString();
 
 //            TermModel list = (TermModel)bindings1[0];
 //            System.out.println("Here is the result:"+list);
 //            if (list.isList()) {
 //                System.out.println(list.getChild(1).toString());
 //            }
-        }
+    }
 }
