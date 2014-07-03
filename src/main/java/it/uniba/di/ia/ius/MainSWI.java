eb030758e2034b222796f708e2116dc8d7020a21
simoruty <simonerutigliano88@gmail.com>
2014-07-03 10:33:00 +0200

    Interprolog

--- src/main/java/it/uniba/di/ia/ius/MainSWI.java
+++ src/main/java/it/uniba/di/ia/ius/MainSWI.java
@@ -1,7 +1,9 @@
 package it.uniba.di.ia.ius;
 
 import it.uniba.di.ia.ius.gui.MainWindow;
+import jpl.Atom;
 import jpl.JPL;
+import jpl.Query;
 
 public class MainSWI {
     public static String swiJPLPath = "/usr/local/lib/swipl-6.6.6/lib/x86_64-linux";
@@ -9,6 +11,8 @@ public class MainSWI {
     public static void main(String args[]) {
         JPL.setNativeLibraryDir(swiJPLPath);
         MainWindow mw = new MainWindow();
+        Query query = new Query(new Atom("version"));
+        System.out.println(query.hasSolution() ? "succeeded" : "failed");
 
 //        new Prolog().method();
     }
