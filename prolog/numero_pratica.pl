015e9e9b35180f0fcc6ff0a04d5073207a54a190
simoruty <simonerutigliano88@gmail.com>
2014-07-24 17:12:16 +0200

    fix valuta

--- prolog/numero_pratica.pl
+++ prolog/numero_pratica.pl
@@ -42,17 +42,17 @@ tag_numero_pratica(X) :-
     kb:next(IDTag1, IDToken2),
     kb:tag(IDTag1, numero(N)),
     kb:appartiene(IDTag1, IDDoc),
-    kb:appartiene(IDToken1, IDDoc),
-    kb:token(IDToken1, 'n'),
-    kb:stessa_frase(IDToken1, IDTag1),
+    kb:appartiene(IDTagn, IDDoc),
+    kb:tag(IDTagn, parola('n')),    
+    kb:stessa_frase(IDTagn, IDTag1),
 
-    findall( Precedente, kb:next(Precedente, IDToken1), ListaPrecedenti ),
+    findall( Precedente, kb:next(Precedente, IDTagn), ListaPrecedenti ),
     findall( Successivo, kb:next(IDTag2, Successivo), ListaSuccessivi ),
 
     atomic_list_concat([N,'/',A], '', X),
 
     atomic_list_concat(['[NUMERO PRATICA] Presenza nel documento di `n` ', N, '/', A],'',Spiegazione),
-    Dipendenze = [IDTag1, IDTag2, IDToken1, IDToken2],
+    Dipendenze = [IDTag1, IDTag2, IDTagn, IDToken2],
     kb:appartiene(IDTag1, IDDoc),  
     kb:appartiene(IDTag2, IDDoc),    
     assertTag(numero_pratica(X), IDDoc, ListaPrecedenti, ListaSuccessivi, Spiegazione, Dipendenze).    
