8af9b621750b35859277b8f8e61cc95708bb36f9
Luciano Quercia <luciano.quercia@gmail.com>
2014-08-02 10:50:57 +0200

    Added fold and py

---
+++ sperimentazioni/aleph/run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import subprocess
+from time import gmtime, strftime
+
+cmd = "chmod +x *.yap"
+subprocess.call(cmd, shell=True)
+#for dataset in ["elsevier", "jmlr", "mlj", "svln"]:
+for dataset in ["elsevier"]:
+    print "Begin " + dataset
+#    for fold in range(10):
+    for fold in range(2):
+        print "---Fold " + str(fold)
+        cmd = "./"+dataset+"_f"+str(fold)+".yap -s50000 -h200000 2>&1 > /dev/null"
+        subprocess.call(cmd, shell=True)
+        strftime("%H:%M:%S", gmtime())
+
