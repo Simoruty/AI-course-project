8b731b6c46d551bcd885b5b28b03ff452e14aa71
simoruty <simonerutigliano88@gmail.com>
2014-08-02 21:19:10 +0200

    fix run python

--- sperimentazioni/progol/run.py
+++ sperimentazioni/progol/run.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
 import subprocess
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime
+from datetime import datetime
 
 cmd = "chmod +x *.yap"
 subprocess.call(cmd, shell=True)
 #for dataset in ["elsevier", "jmlr", "mlj", "svln"]:
 for dataset in ["elsevier"]:
-    print "Begin " + dataset
+    print dataset +" started at "+ strftime("%H:%M:%S", localtime())
+    startDataset = datetime.now()
 #    for fold in range(10):
-    for fold in range(1):
-        print "---Fold " + str(fold)
+    for fold in range(2):
+        startTime = datetime.now()
+        print "---Fold " + str(fold) +" started at "+strftime("%H:%M:%S", localtime())
         cmd = "./"+dataset+"_f"+str(fold)+".yap -s50000 -h200000 2>&1 > /dev/null"
         subprocess.call(cmd, shell=True)
-        strftime("%H:%M:%S", gmtime())
-
+        print "---Fold " + str(fold) +" endend in: "+str(datetime.now()-startTime)
+    print dataset+" ended in "+str(datetime.now()-startDataset)
+    print "---------------------------------------------------"
