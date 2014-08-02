fea487fdf395e9a1a9194e8cec23714b0c1549bc
simoruty <simonerutigliano88@gmail.com>
2014-08-02 12:25:31 +0200

    prova foil

--- sperimentazioni/progol/run.py
+++ sperimentazioni/progol/run.py
@@ -9,7 +9,7 @@ subprocess.call(cmd, shell=True)
 for dataset in ["elsevier"]:
     print "Begin " + dataset
 #    for fold in range(10):
-    for fold in range(2):
+    for fold in range(1):
         print "---Fold " + str(fold)
         cmd = "./"+dataset+"_f"+str(fold)+".yap -s50000 -h200000 2>&1 > /dev/null"
         subprocess.call(cmd, shell=True)
